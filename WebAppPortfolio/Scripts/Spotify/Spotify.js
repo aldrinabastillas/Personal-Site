6dccc3106de252ad465e51a06334187794f4c136
aldrinabastillas <aldrin.abastillas@gmail.com>
2016-10-19 15:23:28 -0400

    Working wireframe

--- WebAppPortfolio/Scripts/Spotify/Spotify.js
+++ WebAppPortfolio/Scripts/Spotify/Spotify.js
@@ -1,7 +1,9 @@
 ﻿angular.module('spotifyApp', [])
+    .run(function () {
+        $('#predictionText').hide(); //hide prediction upon loading page
+    })
     .controller('SpotifyController', ['$scope', '$http', function ($scope, $http) {
         var spotify = this;
-
 
         spotify.songSearch = $('#songSearch').search({
             apiSettings: {
@@ -11,7 +13,8 @@
                         results: []
                     };
 
-                    // translate GitHub API response to work with search
+                    //iterate through results from Spotify
+                    //See https://developer.spotify.com/web-api/search-item/
                     $.each(spotifyResponse.tracks.items, function (i, track) {
                         response.results.push({
                             title: track.name,
@@ -23,15 +26,20 @@
                     return response;
                 },
             },
-            fields: {
+            fields: { //map results from Spotify to semantic-ui API
                 results: 'results',
                 title: 'title',
                 description: 'description',
                 image: 'image'
             },
             minCharacters: 3,
-            onSelect(result, response) {
+            onSelect(result, response) { //callback after song is selcted
                 spotify.IdSearch(result.id);
+                $scope.selectedSong = result.title;
+                $scope.selectedArtist = result.description;
+
+                var url = 'https://embed.spotify.com/?uri=spotify:track:' + result.id;
+                $('#spotifyPlayer').attr('src', url); //change song in the player
             }
         });
 
@@ -42,16 +50,21 @@
                 .success(function (response, status) {
                     var obj = JSON.parse(response);
                     var label = obj.Results.output1[0]['Scored Labels'];
-                    var probability = obj.Results.output1[0]["Scored Probabilities"] * 100;
+                    var probability = obj.Results.output1[0]['Scored Probabilities'] * 100;
+                    var prediction = 'likely';
+
                     if (label == 'False') {
                         probability = 100 - probability; //Given probability is that the label is True, so flip if False
+                        prediction = 'not likely'
                     }
 
-                    $scope.label = 'Prediction: ' + label;
-                    $scope.probability = 'Probability: ' + probability.toFixed(2) + '%';
+                    $scope.prediction = prediction;
+                    $scope.probability = probability.toFixed(2) + '%';
+                    $('#predictionText').show();
                 })
                 .error(function (response, status) {
                     $scope.label = 'Request failed';
+                    $('#predictionText').show();
                 });
         };
 }]);
